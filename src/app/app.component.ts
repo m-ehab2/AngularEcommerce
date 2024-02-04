58d7fcc28ca7fe5638caeb9be75cc01b3b1c8c4f
m-ehab2 <muhamedehab1@gmail.com>
2024-02-04 20:35:17 +0200

    Initialized pageHero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LogoComponent } from './components/shared/logo/logo.component';
+import { PageHeroComponent } from './components/shared/page-hero/page-hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LogoComponent],
+  imports: [RouterOutlet,PageHeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularEcommerce';
+  currentItem = 'Shop';
 }
